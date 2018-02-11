5ad1362ab5d9be6e8e6d90a881cb2e5fedcb6a8a
Kevin Miller <kevin@driftabout.com>
2018-02-10 17:03:58 -0800

    get auth tests working

--- model/auth.js
+++ model/auth.js
@@ -25,16 +25,28 @@ Auth.methods.createHashedpassword = function(password){
     .catch(err => err);
 };
 
+// Auth.methods.comparePasswords = function(password) {
+//   debug('password', password);
+//   return bcrypt.compare(password, this.password)
+//     .then(valid => {
+//       debug('valid before:', valid);
+//       if(!valid) return Promise.reject(new Error('Authorization Error: invalid password'));
+//       debug('valid after:', valid);
+//       return Promise.resolve(this);
+//     })
+//     .catch(err => err);
+// };
+
 Auth.methods.comparePasswords = function(password) {
-  debug('password', password);
-  return bcrypt.compare(password, this.password)
-    .then(valid => {
+  return new Promise((resolve, reject) => { 
+    bcrypt.compare(password, this.password, (err, valid) => {
       debug('valid before:', valid);
-      if(!valid) return new Error('Authorization Error: invalid password');
+      if(err) return reject(err);
+      if(!valid) return reject(new Error('Authorization Error: invalid password'));
       debug('valid after:', valid);
-      return this;
-    })
-    .catch(err => err);
+      return resolve(this);
+    });
+  });
 };
 
 Auth.methods.createCompHash = function() {
--- route/route-auth.js
+++ route/route-auth.js
@@ -6,16 +6,18 @@ const bodyParser = require('body-parser').json();
 const errorHandler = require('../lib/error-handler');
 const Auth = require('../model/auth');
 
+debug('route-auth');
+
+const ERROR_MESSAGE = 'Authorization Failed';
+
 module.exports = function(router) {
 
   router.get('/signin', basicAuth, (req, res) => {
-    debug('req.auth', req.auth);
     Auth.findOne({username: req.auth.username})
       .then(user => {
-        debug('user', user);
-        if(!user) return new Error('Invalid user:  User not found');
-        return user ? user.comparePasswords(req.auth.password) : new Error('Invalid user:  User not found');
-      }).then(user => {
+        return user ? user.comparePasswords(req.auth.password) : Promise.reject(new Error(ERROR_MESSAGE));
+      })
+      .then(user => {
         delete req.headers.authorization;
         delete req.auth.password;
         return user;
@@ -25,12 +27,11 @@ module.exports = function(router) {
         res.status(200).json(jwt);
       })
       .catch(err => errorHandler(err, res));
-
   });
 
   router.post('/signup', bodyParser, (req, res) => {
     debug('req.body', req.body);
-    if(!req.body.hasOwnProperty('username') || !req.body.hasOwnProperty('password')) return errorHandler(new Error('Bad Request'),res);
+    if(!req.body.username || !req.body.password) return errorHandler(new Error('Bad Request'),res);
     let pswd = req.body.password;
     delete req.body.password;
     let user = new Auth(req.body);
